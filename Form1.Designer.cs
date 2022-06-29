3a9fa6038f310707bc3bf260a0821d9bf2759c7e
ViniciusCarvalhoo <viniciuscarvalhoo1907@gmail.com>
2022-06-29 19:36:35 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Triangulo
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Triangulo));
+            this.lblBase = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblBase
+            // 
+            this.lblBase.AutoSize = true;
+            this.lblBase.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBase.Location = new System.Drawing.Point(7, 57);
+            this.lblBase.Name = "lblBase";
+            this.lblBase.Size = new System.Drawing.Size(82, 30);
+            this.lblBase.TabIndex = 0;
+            this.lblBase.Text = "Base: ";
+            this.lblBase.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(7, 124);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(113, 30);
+            this.lblAltura.TabIndex = 1;
+            this.lblAltura.Text = "Altura:";
+            this.lblAltura.Click += new System.EventHandler(this.lblAltura_Click);
+            // 
+            // txtBase
+            // 
+            this.txtBase.Location = new System.Drawing.Point(123, 57);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(290, 20);
+            this.txtBase.TabIndex = 2;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(126, 124);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(287, 20);
+            this.txtAltura.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(669, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "DIGITE A BASE E A ALTURA DO TRIANGULO PARA DESCOBRIR A AREA";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(460, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 76);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(13, 230);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(162, 30);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "Resultado: ";
+            this.lblResultado.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(182, 239);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(231, 20);
+            this.txtResultado.TabIndex = 7;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Triangulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp2.Properties.Resources._2b0baa50_32f5_4c95_9365_a18c83a2b879;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtBase);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblBase);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Triangulo";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBase;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Triangulo : Form
+    {
+        public Triangulo()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void lblAltura_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double basee = double.Parse(txtBase.Text);
+            double alturaa = double.Parse(txtAltura.Text);
+
+            double resultado = basee * alturaa / 2;
+
+            txtResultado.Text = resultado.ToString();
+        }
+    }
+}
